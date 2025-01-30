a4b30ac4020b68be5a33fcaa8ed35c1da9e45a2d
Felipe <tackberry@bol.com.br>
2025-01-30 16:25:28 -0300

    Prototipo de Perguntas e Respostas

---
+++ jogo.py
@@ -0,0 +1,6 @@
+print("Qual a capital do Brasil? A) Rio de Janeiro B) Brasilia C) São Paulo D) Salvador")
+resposta = input("escolha a alternativa correta: ")
+if resposta == "B":
+    print("Acertou!")
+else:
+    print("Errou!")
